7a57e8efd3118742f100daaab7b089b81d6fd8bf
Artiom Mudric <artiommudric@gmail.com>
2024-03-03 04:03:54 +0200

    asdasda

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/profile'
   devise_for :user, controllers: {
   sessions: 'user/sessions',
   registrations: 'user/registrations'
   }
+  ge '/u/:id', to: 'users#profile', as 'user'
+
   resources :posts
     get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
